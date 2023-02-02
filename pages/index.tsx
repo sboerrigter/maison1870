52eaeb21285c6d705ec9c51e9d08677a0cfc38c9
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2023-02-02 23:57:49 +0100

    Button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 import Wrap from "@/components/Wrap";
 
 export default function Home() {
@@ -52,6 +53,37 @@ export default function Home() {
             distance. And you can visit all main sights and attractions in the
             Perigord noir within a 30 minute drive from the apartment.
           </p>
+
+          <Link
+            className="
+              bg-emerald-400 hover:bg-emerald-600 text-white
+              font-semibold
+              inline-flex items-center justify-center
+              px-8 py-3 rounded-full
+              shadow-lg shadow-emerald-500/50
+
+            "
+            href="https://www.airbnb.com/rooms/781840949977429677"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="mr-3"
+            >
+              <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
+              <line x1="16" y1="2" x2="16" y2="6"></line>
+              <line x1="8" y1="2" x2="8" y2="6"></line>
+              <line x1="3" y1="10" x2="21" y2="10"></line>
+            </svg>
+            Availability & bookings
+          </Link>
         </Wrap>
       </main>
     </>
