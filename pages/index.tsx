849dbcafb8aa6e2330af080111c8e4ecba01b8af
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2023-02-03 00:22:37 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -67,7 +67,7 @@ export default function Home() {
 
           <Link
             className="
-              bg-emerald-400 hover:bg-emerald-600 text-white
+              bg-emerald-400 hover:bg-emerald-500 text-white
               font-semibold
               inline-flex items-center justify-center
               px-8 py-3 rounded-full
