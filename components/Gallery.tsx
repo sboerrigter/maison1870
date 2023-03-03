5f79b8cffa68e8165be450239b859a9da4971801
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2023-03-03 23:02:06 +0100

    :construction: Gallery

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -3,9 +3,9 @@ import Wrap from "@/components/Wrap";
 import { useState } from "react";
 
 export default function Gallery() {
-  const images = [
-    1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-  ];
+  const images = [...Array(19).keys()].map((number) => {
+    return number + 1;
+  });
   const [current, setCurrent] = useState(1);
 
   const previous = () => {
@@ -19,23 +19,32 @@ export default function Gallery() {
   return (
     <section className="my-section">
       <Wrap className="max-w-[880px]">
-        <Image
-          alt="Maison 1870"
-          className="w-full aspect-[4/3] bg-gray-900 mb-5 rounded-lg object-contain"
-          src={`/photos/${current}.jpg`}
-          width={800}
-          height={600}
-          sizes="(max-width: 880px) 100vw, 800px"
-        />
+        <div className="w-full aspect-[4/3] bg-gray-900 mb-3 md:mb-5 rounded-lg overflow-hidden relative">
+          {images.map((number) => (
+            <Image
+              alt={`Maison 1870, photo ${number}`}
+              className={`
+                  object-contain transition-opacity duration-150
+                  absolute w-full h-full left-0 top-0
+                ${current == number ? "opacity-100" : "opacity-0"}
+              `}
+              height={600}
+              key={number}
+              sizes="(max-width: 880px) 100vw, 800px"
+              src={`/photos/${number}.jpg`}
+              width={800}
+            />
+          ))}
+        </div>
       </Wrap>
 
-      <div className="flex flex-nowrap overflow-x-auto gap-5 p-5 md:px-10 pb-10 -mb-10">
+      <div className="flex flex-nowrap overflow-x-auto gap-4 md:gap-5 p-5 md:px-10 pb-10 -mb-10">
         {images.map((number) => (
           <Image
-            alt="Maison 1870"
+            alt={`Maison 1870, photo ${number}`}
             className={`
               w-[120px] md:w-[180px] aspect-square rounded-lg object-cover cursor-pointer
-              transition-all duration-100 ease-out
+              transition-all duration-150 ease-out
               ${
                 current == number
                   ? "scale-110 shadow-lg shadow-black/20"
