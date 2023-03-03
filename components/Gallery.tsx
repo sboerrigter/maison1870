ac1d271865e7813491d18a4e7795a3333861ae42
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2023-03-03 22:45:09 +0100

    :construction: Gallery

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -1,51 +1,56 @@
 import Image from "next/image";
 import Wrap from "@/components/Wrap";
+import { useState } from "react";
 
 export default function Gallery() {
+  const images = [
+    1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+  ];
+  const [current, setCurrent] = useState(1);
+
+  const previous = () => {
+    setCurrent(current - 1);
+  };
+
+  const next = () => {
+    setCurrent(current + 1);
+  };
+
   return (
     <section className="my-section">
       <Wrap className="max-w-[880px]">
         <Image
           alt="Maison 1870"
-          className="w-full rounded-xl mb-5"
-          src="/photos/01.jpg"
+          className="w-full aspect-[4/3] bg-gray-900 mb-5 rounded-lg object-contain"
+          src={`/photos/${current}.jpg`}
           width={800}
           height={600}
           sizes="(max-width: 880px) 100vw, 800px"
         />
-
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-5">
-          {[
-            "01",
-            "02",
-            "03",
-            "04",
-            "05",
-            "06",
-            "07",
-            "08",
-            "10",
-            "11",
-            "12",
-            "13",
-            "14",
-            "15",
-            "16",
-            "17",
-            "18",
-            "19",
-          ].map((number) => (
-            <Image
-              alt="Maison 1870"
-              className="w-full rounded-xl object-cover aspect-square"
-              height={185}
-              key={number}
-              src={`/photos/${number}.jpg`}
-              width={185}
-            />
-          ))}
-        </div>
       </Wrap>
+
+      <div className="flex flex-nowrap overflow-x-auto gap-5 p-5 pb-20 -mb-20">
+        {images.map((number) => (
+          <Image
+            alt="Maison 1870"
+            className={`
+              w-[120px] md:w-[180px] aspect-square rounded-lg object-cover cursor-pointer
+              transition-all duration-100 ease-out
+              ${
+                current == number
+                  ? "scale-105 shadow-xl shadow-black/20"
+                  : "shadow-sm shadow-transparent"
+              }
+            `}
+            draggable={false}
+            height={180}
+            key={number}
+            onClick={() => setCurrent(number)}
+            src={`/photos/${number}.jpg`}
+            width={180}
+          />
+        ))}
+      </div>
     </section>
   );
 }
