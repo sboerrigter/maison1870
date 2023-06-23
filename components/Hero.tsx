11a21dbf0a90c9ed57b148ad469c721175097f19
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2023-06-23 14:19:34 +0200

    :lipstick: Give me a blue sky and 🍎 Safari aspect ratio bug

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,7 +5,7 @@ import Wrap from "@/components/Wrap";
 
 export default function Hero() {
   return (
-    <header className="bg-gray-600 w-full aspect-[4/3] min-h-[360px] max-h-[75vh] relative">
+    <header className="bg-gray-600 w-full h-[66.67vw] min-h-[360px] max-h-[75vh] relative">
       <Image
         alt="Holiday apartment in Sarlat&#8209;la&#8209;Canéda"
         className="w-full h-full absolute left-0 top-0 object-cover z-0"
@@ -16,7 +16,7 @@ export default function Hero() {
         sizes="100vw"
       />
 
-      <div className="w-full h-full absolute left-0 top-0 bg-gradient-to-b from-black/40 to-transparent" />
+      <div className="w-full h-full absolute left-0 top-0 bg-gradient-to-b from-blue-900/75 via-transparent" />
 
       <Wrap className="max-w-[1280px] relative flex justify-between items-end pt-6 2xs:pt-8 md:pt-10">
         <Logo />
