68c8ce96da1f47f6f61f29a42dd92c665a750f24
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2023-06-23 16:10:05 +0200

    :lipstick: Hosts image

--- components/Hosts.tsx
+++ components/Hosts.tsx
@@ -7,13 +7,17 @@ export default function Hosts() {
   return (
     <section className="mt-section flex bg-gradient-to-b from-transparent to-emerald-50 pb-12">
       <Wrap className="mb-section flex max-w-[1160px] flex-col-reverse items-center gap-[60px] lg:flex-row xl:gap-[120px]">
-        <Image
-          alt="Your hosts Hannah & Sjoerd"
-          className="rounded-full shadow lg:w-1/2"
-          height={480}
-          src="/photos/hannah-sjoerd.jpg"
-          width={480}
-        />
+        <div className="mx-8 xs:mx-12 lg:mx-0 lg:w-1/2">
+          {" "}
+          <Image
+            alt="Your hosts Hannah & Sjoerd"
+            className="rounded-full shadow"
+            height={480}
+            src="/photos/hannah-sjoerd.jpg"
+            width={480}
+          />
+        </div>
+
         <div className="text-center lg:w-1/2 lg:text-left">
           <h2>
             Your hosts Hannah&nbsp;<span className="font-serif">&amp;</span>
